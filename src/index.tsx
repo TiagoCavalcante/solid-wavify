5a549bf81b89e377c9c26e2599e09a7c37ed6531
Tiago Cavalcante Trindade <tiagotrindade111@gmail.com>
2022-05-05 20:51:18 -0300

    Convert code in src to solid-js

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
-import WaveBase from './wave'
+import WaveBase from './wave';
+import type { BaseWaveProps, WaveHTMLProps } from './wave';
 
 const defaults = {
   height: 20,
   amplitude: 20,
   speed: 0.15,
   points: 3,
-}
+};
+
+export type WaveProps = WaveHTMLProps
+  & Pick<BaseWaveProps, "fill" | "paused">
+  & { options: Omit<BaseWaveProps, "fill" | "paused">; };
 
 export default function Wave({
   fill = "#fff",
   paused = false,
   options,
   ...rest
-}) {
+}: WaveProps) {
   return (
     <WaveBase
       fill={fill}
@@ -21,5 +26,5 @@ export default function Wave({
       {...options}
       {...rest}
     />
-  )
+  );
 }
--- src/wave.tsx
+++ src/wave.tsx
@@ -1,32 +1,65 @@
-import { onCleanup, onMount } from 'solid-js';
+import { createSignal, onCleanup, onMount } from 'solid-js';
+import type { JSX, PropsWithChildren } from 'solid-js';
 
-export default function Wave(props) {
-  this._container = React.createRef();
-  this.state = { path: '' };
-  this._lastUpdate = 0;
-  this._elapsed = 0;
-  this._step = 0;
+export type Point = {
+  x: number,
+  y: number;
+};
 
-  function _calculateWavePoints() {
-    const points = [];
-    for (let i = 0; i <= Math.max(this.props.points, 1); i++) {
+export type BaseWaveProps = {
+  style: JSX.CSSProperties,
+  className: string,
+  fill: JSX.IntrinsicElements["path"]["fill"],
+  paused: Boolean,
+  id: string,
+  ref: HTMLDivElement | null,
+  height: number,
+  amplitude: number,
+  speed: number,
+  points: number,
+};
+
+export type WaveHTMLProps = Omit<
+  JSX.IntrinsicElements["path"],
+  keyof BaseWaveProps | "children"
+>;
+
+export type WaveProps = Partial<BaseWaveProps & WaveHTMLProps>;
+
+export default function Wave({
+  amplitude = 20,
+  height = 20,
+  points = 3,
+  speed = 0.15,
+  ...props
+}: PropsWithChildren<WaveProps>) {
+  const [path, setPath] = createSignal('');
+
+  let lastUpdate = 0;
+  let elapsed = 0;
+  let step = 0;
+  let frameId = 0;
+
+  function calculateWavePoints() {
+    const pointList = [];
+    for (let i = 0; i <= Math.max(points, 1); i++) {
       const scale = 100;
-      const x = i / this.props.points * this._width();
-      const seed = (this._step + (i + i % this.props.points)) * this.props.speed * scale;
-      const height = Math.sin(seed / scale) * this.props.amplitude;
-      const y = Math.sin(seed / scale) * height + this.props.height;
-      points.push({ x, y });
+      const x = i / points * getWidth();
+      const seed = (step + (i + i % points)) * speed * scale;
+      const currentHeight = Math.sin(seed / scale) * amplitude;
+      const y = Math.sin(seed / scale) * currentHeight + height;
+      pointList.push({ x, y });
     }
-    return points;
+    return pointList;
   }
 
-  function _buildPath(points) {
+  function buildPath(points: Array<Point>) {
     let svg = `M ${points[0].x} ${points[0].y}`;
     const initial = {
       x: (points[1].x - points[0].x) / 2,
       y: (points[1].y - points[0].y) + points[0].y + (points[1].y - points[0].y)
     };
-    const cubic = (a, b) => ` C ${a.x} ${a.y} ${a.x} ${a.y} ${b.x} ${b.y}`;
+    const cubic = (a: Point, b: Point) => ` C ${a.x} ${a.y} ${a.x} ${a.y} ${b.x} ${b.y}`;
     svg += cubic(initial, points[1]);
     let point = initial;
     for (let i = 1; i < points.length - 1; i++) {
@@ -36,53 +69,48 @@ export default function Wave(props) {
       };
       svg += cubic(point, points[i + 1]);
     }
-    svg += ` L ${_width()} ${this._height()}`;
-    svg += ` L 0 ${_height()} Z`;
+    svg += ` L ${getWidth()} ${getHeight()}`;
+    svg += ` L 0 ${getHeight()} Z`;
     return svg;
   }
 
-  const _width = () => this._container.current.offsetWidth;
-  const _height = () => this._container.current.offsetHeight;
+  const getWidth = () => ref!.offsetWidth;
+  const getHeight = () => ref!.offsetHeight;
 
-  function _redraw() {
-    this.setState({
-      path: _buildPath(_calculateWavePoints())
-    });
+  function redraw() {
+    setPath(buildPath(calculateWavePoints()));
   }
 
-  function _draw() {
-    if (!this.props.paused) {
-      const now = new Date();
-      this._elapsed += (now - this._lastUpdate);
-      this._lastUpdate = now;
+  function draw() {
+    if (!props.paused) {
+      const now = new Date().getTime();
+      elapsed += now - lastUpdate;
+      lastUpdate = now;
     }
     const scale = 1000;
-    this._step = this._elapsed * Math.PI / scale;
-    _redraw();
+    step = elapsed * Math.PI / scale;
+    redraw();
   }
 
-  function _update() {
-    _draw();
-    if (this._frameId) {
-      _resume();
-    }
+  function update() {
+    draw();
+
+    if (frameId) resume();
   }
 
-  function _resume() {
-    this._frameId = window.requestAnimationFrame(_update);
-    this._lastUpdate = new Date();
+  function resume() {
+    frameId = window.requestAnimationFrame(update);
+    lastUpdate = new Date().getTime();
   }
 
   onMount(() => {
-    if (!this._frameId) {
-      _resume();
-    }
-  })
+    if (!frameId) resume();
+  });
 
   onCleanup(() => {
-    window.cancelAnimationFrame(this._frameId);
-    this._frameId = 0;
-  })
+    window.cancelAnimationFrame(frameId);
+    frameId = 0;
+  });
 
   const {
     style,
@@ -91,21 +119,25 @@ export default function Wave(props) {
     paused,
     children,
     id,
-    d,
     ref,
-    height,
-    amplitude,
-    speed,
-    points,
     ...rest
   } = props;
 
   return (
-    <div style={{ width: '100%', display: 'inline-block', ...style }}
-      className={className} id={id} ref={this._container}>
-      <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <div
+      style={{ width: '100%', display: 'inline-block', ...style }}
+      className={className}
+      id={id}
+      ref={ref!}
+    >
+      <svg
+        width="100%"
+        height="100%"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         {children}
-        <path d={this.state.path} fill={fill} {...rest} />
+        <path d={path()} fill={fill} {...rest} />
       </svg>
     </div>
   );
